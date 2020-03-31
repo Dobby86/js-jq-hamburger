3b82138b9e8113fdd046ee4ab8ed038c40a0e9e0
marco <dobermannpoker86@gmail.com>
2020-03-31 18:32:48 +0200

    bonus finito

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -1,1 +1,14 @@
 // console.log("ciao");
+$(".tasti").hide();
+
+$("#dio").click (
+    function(){
+        $(".tasti").show()
+
+        })
+
+        $("#dio").dblclick (
+        function(){
+            $(".tasti").hide()
+
+        }) ;
